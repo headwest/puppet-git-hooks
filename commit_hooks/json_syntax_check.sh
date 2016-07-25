c6e3d44e687e070686cf1eaef9bce3b617cea8ef
Mark Turner <mturner@redhat.com>
2016-07-25 11:54:10 -0400

    switched to /bin/sh

--- commit_hooks/json_syntax_check.sh
+++ commit_hooks/json_syntax_check.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # This script expects $1 to be passed and for $1 to be the filesystem location
 # to a json file for which it will run syntax checks against.
 
 syntax_errors=0
-error_msg=$(mktemp /tmp/error_msg_json-syntax.XXXXX)
+script_name=$(basename ${0})
+error_msg=$(mktemp /tmp/error_msg_${script_name}.XXXXX)
 
 if [ $2 ]; then
     module_path=$(echo $1 | sed -e 's|'$2'||')
@@ -13,12 +14,12 @@ else
 fi
 
 # Check json file syntax
-echo -e "$(tput setaf 6)Checking json syntax for $module_path...$(tput sgr0)"
+echo "$(tput setaf 6)Checking json syntax for $module_path...$(tput sgr0)"
 ruby -e "require 'rubygems'; require 'json'; JSON.parse(File.read('$1'))" 2> $error_msg > /dev/null
 if [ $? -ne 0 ]; then
     cat $error_msg | sed -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/"
     syntax_errors=`expr $syntax_errors + 1`
-    echo -e "$(tput setaf 1)Error: json syntax error in $module_path (see above)$(tput sgr0)"
+    echo "$(tput setaf 1)Error: json syntax error in $module_path (see above)$(tput sgr0)"
 fi
 rm -f $error_msg
 
@@ -28,13 +29,14 @@ if which metadata-json-lint > /dev/null 2>&1; then
         if [ $? -ne 0 ]; then 
             cat $error_msg | sed -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/"
             syntax_errors=`expr $syntax_errors + 1`
-            echo -e "$(tput setaf 1)Error: json syntax error in $module_path (see above)$(tput sgr0)"
+            echo "$(tput setaf 1)Error: json syntax error in $module_path (see above)$(tput sgr0)"
         fi
     fi
 fi
 
 if [ "$syntax_errors" -ne 0 ]; then
-    echo -e "$(tput setaf 1)Error: $syntax_errors syntax error(s) found in json file.  Commit will be aborted.$(tput sgr0)"
+    echo
+    echo "$(tput setaf 1)*** Commit will be aborted.$(tput sgr0)"
     exit 1
 fi
 
